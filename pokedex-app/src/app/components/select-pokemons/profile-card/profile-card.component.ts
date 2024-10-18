d9cef642e0ad7d9cb625fbd89298e9923969abe9
ajla <josephelop07@gmail.com>
2024-10-18 16:38:58 -0600

    Profile data get
    Card data load data from input

--- pokedex-app/src/app/components/select-pokemons/profile-card/profile-card.component.ts
+++ pokedex-app/src/app/components/select-pokemons/profile-card/profile-card.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component , Input, OnInit} from '@angular/core';
+import { Trainer } from 'src/app/models/trainer';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.scss']
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+
+  @Input() trainer: Trainer;
+  birthdate: Date;
+  defaultImageUrl: string = "../../../assets/icons/default_avatar.png";
+  profileIcon: string | ArrayBuffer;
+  profileIconName: string;
+  isPreview: boolean = false;
+
+ngOnInit(): void {
+  
+  this.birthdate = new Date(this.trainer.birthdate);
+}
+
+  getHobbieName(input: string): string {
+    switch (input) {
+      case "soccer":
+        return "Jugar Fútbol";
+      case "basquetball":
+        return "Jugar Basquetball";
+      case "tennis":
+        return "Jugar Tennis";
+      case "voleiball":
+        return "Jugar Voleiball";
+      case "fifa":
+        return "Jugar Fifa";
+      case "gaming":
+        return "Jugar Videojuegos";
+      default:
+        return "N/A";
+    }
+  }
+
 
 }
--- pokedex-app/src/app/components/select-pokemons/select-pokemons/select-pokemons.component.ts
+++ pokedex-app/src/app/components/select-pokemons/select-pokemons/select-pokemons.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Trainer } from 'src/app/models/trainer';
+import { TrainerService } from 'src/app/services/trainer.service';
+
 
 @Component({
   selector: 'app-select-pokemons',
   templateUrl: './select-pokemons.component.html',
   styleUrls: ['./select-pokemons.component.scss']
 })
-export class SelectPokemonsComponent {
+export class SelectPokemonsComponent implements OnInit{
+
+  isFetching: boolean = false;
+  trainer: Trainer;
+  private profileDataSubscription: Subscription;
+
+  constructor(private activatedRoute: ActivatedRoute, private trainerService: TrainerService, ){ }
+
+  ngOnInit(): void {
+    this.getProfileDataSubscription();
+  }
+
+  private getProfileDataSubscription(): void {
+    this.profileDataSubscription = this.activatedRoute.data.subscribe(({trainer}: { trainer: Trainer })=>{
+      this.trainer = trainer;
+      setTimeout(() => {
+        this.isFetching = false;
+      }, 1000);  //simular 1 segundo de tiempo de carga de datos
+      // this.preselectPokemons();
+    });
+  }
 
 }
